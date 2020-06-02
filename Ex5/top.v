d8e36aa272db9dc46e06a553eff1be43f7e01b43
Olivia <sedm6132@ox.ac.uk>
2020-06-02 17:38:26 +0000

    start Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Olivia Banks
+// Date: 6/2/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -13,3 +13,27 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+module lights (
+	input clk,
+	output red,
+	output amber,
+	output green
+);
+
+always @(posedge clk)
+	if(red == 1)
+		if(amber == 0)
+			amber <= 1;
+		else
+			red <= 0;
+			amber <= 0;
+			green <= 1;
+	else
+		if(green == 1)
+			amber <= 1;
+			green <= 0;
+		else
+			red <= 1;
+			amber <= 0;
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,56 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Olivia Banks
+// Date: 6/2/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+parameter CLK_PERIOD = 10;
+
+// Registers and wires
+reg clk;
+reg err;
+reg red;
+reg amber;
+reg green;
+
+// Clock generation
+initial
+begin
+    clk = 1'b0;
+    forever
+      #(CLK_PERIOD/2) clk=~clk;
+end
+
+// Tests
+initial begin
+
+	forever begin
+	
+	end
+end
+
+// Finish tests
+initial begin
+  #80
+  if (err==0)
+    $display("***TEST PASSED! :) ***");
+  $finish;
+end
+
+// Instantiate lights module
+lights top (
+	.clk (clk),
+	.red (red),
+	.amber (amber),
+	.green (green)
+);
+
+endmodule
