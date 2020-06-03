ab494427554647ebe428f359e162deaaa63bccf3
Olivia <sedm6132@ox.ac.uk>
2020-06-03 14:14:27 +0000

    start ex 7

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Olivia Banks
+// Date: 6/3/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -13,4 +13,14 @@
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+module multiplier (
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	output [4:0] result
+	);
+	
+	
+	
+endmodule
 
