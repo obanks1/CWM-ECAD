c1f8ce31149176af8eb850e277a59a5cd99fd0d3
Olivia <sedm6132@ox.ac.uk>
2020-06-04 14:52:08 +0000

    start 9

--- Ex9/top.v
+++ Ex9/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #9 
-// Student Name:
-// Date: 
+// Student Name: Olivia Banks
+// Date: 6/4/2020
 //
 //  Description: In this exercise, you need to design a module that lights leds
 //  at different frequencies
@@ -17,26 +17,41 @@
 module top(
     input clk_p,
     input clk_n,
-     //Todo: add all other ports besides clk_n and clk_p 
+    //Todo: add all other ports besides clk_n and clk_p 
+	input rst_n,
+	input button,
+	output led_0,
+	output led_1,
+	output led_2
    );
     
 
-   /* clock infrastructure, do not modify */
+	/* clock infrastructure, do not modify */
         wire clk_ibufds;
 
     IBUFDS IBUFDS_sysclk (
 	.I(clk_p),
 	.IB(clk_n),
 	.O(clk_ibufds)
-);
+	);
 
      wire clk; //use this signal as a clock for your design
         
      BUFG bufg_clk (
-	.I  (clk_ibufds),
-	.O  (clk)
+	 .I  (clk_ibufds),
+	 .O  (clk)
       );
 
-//Add logic here
+	//Add logic here
+
+	always @(posedge clk or posedge rst or posedge button)
+		if(rst == 0 || button == 1) begin
+			led_0 <= 0;
+			led_1 <= 0;
+			led_2 <= 0;
+		end
+		else begin
+			if(
+		end
 
 endmodule
